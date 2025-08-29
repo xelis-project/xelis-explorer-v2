2425205de3b36af063cd845d13c22069c4bd7b3b
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-08-29 09:27:15 -0400

    parse addr string to detach port and get more info like ipv4/ipv6

---
+++ src/client/utils/parse_addr.ts
@@ -0,0 +1,19 @@
+interface AddrInfo {
+    ip: string;
+    port: string;
+    type: "ipv4" | "ipv6";
+}
+
+export const parse_addr = (addr: string): AddrInfo | undefined => {
+    // ipv4
+    const ipv4_matches = /^([0-9.]+):(\d+)$/.exec(addr);
+    if (ipv4_matches) {
+        return { ip: ipv4_matches[1], port: ipv4_matches[2], type: `ipv4` };
+    }
+
+    // ipv6
+    const ipv6_matches = /^\[([0-9a-f:]+)\]:(\d+)$/i.exec(addr);
+    if (ipv6_matches) {
+        return { ip: ipv6_matches[1], port: ipv6_matches[2], type: `ipv6` };
+    }
+}
