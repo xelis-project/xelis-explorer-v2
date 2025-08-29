b430d64db5a3e3aa8d6aee59cf4c6c35ab5f9753
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-08-29 10:48:04 -0400

    impl peer map

---
+++ src/client/components/peers_map/peers_map.ts
@@ -0,0 +1,57 @@
+import { Peer } from "@xelis/sdk/daemon/types";
+import type * as leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { fetch_geo_location } from "../../utils/fetch_geo_location";
+import { parse_addr } from "../../utils/parse_addr";
+
+export class PeersMap {
+    element: HTMLDivElement;
+    map!: leaflet.Map;
+
+    constructor() {
+        this.element = document.createElement(`div`);
+        this.setup_map();
+    }
+
+    async setup_map() {
+        const leaflet = await import("leaflet");
+        this.map = leaflet.map(this.element).setView([20, 0], 2);
+        leaflet.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        }).addTo(this.map);
+    }
+
+    async set(peers: Peer[]) {
+        const leaflet = await import("leaflet");
+        const peers_addr = peers.map(peer => {
+            const addr = parse_addr(peer.addr);
+            if (addr) return { peer, addr };
+        }).filter(p => p !== undefined);
+        const ips = peers_addr.map(p => p.addr.ip);
+
+        const geo_locations = await fetch_geo_location(ips);
+        Object.keys(geo_locations).forEach((key) => {
+            const peer_addr = peers_addr.find(p => p.addr.ip === key);
+            if (!peer_addr) return;
+
+            const location = geo_locations[key];
+            leaflet.marker([location.latitude, location.longitude])
+                .addTo(this.map)
+                .bindPopup(`<div>
+                    <div>
+                        <div>${peer_addr.peer.version}</div>
+                        <div>${location.city}, ${location.country}</div>
+                        <div>${location.region}</div>
+                    </div>
+                </div>`);
+        });
+    }
+
+    add(peer: Peer) {
+
+    }
+
+    remove(peer: Peer) {
+
+    }
+}
