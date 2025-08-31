944e2f2ef933baddda64ac0499412b3bcfaaa90d
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-08-31 11:54:42 -0400

    throw if invalid addr

--- src/client/utils/parse_addr.ts
+++ src/client/utils/parse_addr.ts
@@ -4,16 +4,18 @@ interface AddrInfo {
     type: "ipv4" | "ipv6";
 }
 
-export const parse_addr = (addr: string): AddrInfo | undefined => {
+export const parse_addr = (addr: string) => {
     // ipv4
     const ipv4_matches = /^([0-9.]+):(\d+)$/.exec(addr);
     if (ipv4_matches) {
-        return { ip: ipv4_matches[1], port: ipv4_matches[2], type: `ipv4` };
+        return { ip: ipv4_matches[1], port: ipv4_matches[2], type: `ipv4` } as AddrInfo;
     }
 
     // ipv6
     const ipv6_matches = /^\[([0-9a-f:]+)\]:(\d+)$/i.exec(addr);
     if (ipv6_matches) {
-        return { ip: ipv6_matches[1], port: ipv6_matches[2], type: `ipv6` };
+        return { ip: ipv6_matches[1], port: ipv6_matches[2], type: `ipv6` } as AddrInfo;
     }
+
+    throw new Error("Invalid address");
 }
