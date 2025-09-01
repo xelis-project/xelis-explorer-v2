3a8be5a16ca4c4f237ae0fc5b49df5d6c5197d5b
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-01 12:03:03 -0400

    update new mempool tx

--- src/client/pages/mempool/mempool.ts
+++ src/client/pages/mempool/mempool.ts
@@ -1,4 +1,4 @@
-import { Block, RPCEvent as DaemonRPCEvent, MempoolTransactionSummary } from "@xelis/sdk/daemon/types";
+import { Block, RPCEvent as DaemonRPCEvent, MempoolTransactionSummary, Transaction } from "@xelis/sdk/daemon/types";
 import { XelisNode } from "../../app/xelis_node";
 import { Master } from "../../components/master/master";
 import { TxBlock } from "../../components/tx_item/tx_item";
@@ -24,14 +24,17 @@ export class MempoolPage extends Page {
     mempool_search: MempoolSearch;
 
     page_data: {
-        blocks: Block[]
+        top_block?: Block;
+        blocks: Block[];
+        mempool_txs: Transaction[];
     }
 
     constructor() {
         super();
 
         this.page_data = {
-            blocks: []
+            blocks: [],
+            mempool_txs: []
         };
 
         this.master = new Master();
@@ -61,19 +64,36 @@ export class MempoolPage extends Page {
     }
 
 
-    on_transaction_added_in_mempool = (new_tx?: MempoolTransactionSummary, err?: Error) => {
+    on_transaction_added_in_mempool = async (new_tx?: MempoolTransactionSummary, err?: Error) => {
         if (new_tx) {
+            const top_block = this.page_data.top_block;
 
+            if (top_block) {
+                const future_block = { height: top_block.height + 1, timestamp: new_tx.first_seen } as Block;
+
+                const node = XelisNode.instance();
+                const tx = await node.ws.methods.getTransaction(new_tx.hash);
+
+                this.page_data.mempool_txs.push(tx);
+
+                const tx_block = { tx: tx, block: future_block } as TxBlock;
+                this.mempool_txs_list.prepend_tx(tx_block);
+                this.mempool_state.set(this.page_data.mempool_txs, top_block);
+            }
         }
     }
 
     on_new_block = async (new_block?: Block, err?: Error) => {
         if (new_block) {
-            this.page_data.blocks.shift();
-            this.page_data.blocks.push(new_block);
+            const { top_block, blocks, mempool_txs } = this.page_data;
+            if (top_block) {
+                blocks.shift();
+                blocks.push(new_block);
 
-            this.mempool_chart.blocks_txs.set(this.page_data.blocks);
-            this.mempool_state.reset();
+                this.page_data.mempool_txs = [];
+                this.mempool_chart.blocks_txs.set(this.page_data.blocks);
+                this.mempool_state.set(mempool_txs, top_block);
+            }
         }
     }
 
@@ -81,7 +101,6 @@ export class MempoolPage extends Page {
         const node = XelisNode.instance();
         node.ws.methods.closeListener(DaemonRPCEvent.TransactionAddedInMempool, this.on_transaction_added_in_mempool);
         node.ws.methods.listen(DaemonRPCEvent.NewBlock, this.on_new_block);
-
     }
 
     async listen_node_events() {
@@ -101,11 +120,15 @@ export class MempoolPage extends Page {
         const node = XelisNode.instance();
 
         const top_block = await node.rpc.getTopBlock();
-        const mempool = await node.rpc.getMemPool();
+        this.page_data.top_block = top_block;
+
+        const mempool_txs = await node.rpc.getMemPool() as any; // TODO fix return type
+        this.page_data.mempool_txs = mempool_txs as Transaction[];
+
         const blocks = await fetch_blocks(top_block.height, 50);
         this.page_data.blocks = blocks;
 
-        this.mempool_state.set(mempool, top_block);
+        this.mempool_state.set(this.page_data.mempool_txs, top_block);
         this.mempool_txs_list.set([]);
         this.mempool_chart.blocks_txs.set(blocks);
     }
