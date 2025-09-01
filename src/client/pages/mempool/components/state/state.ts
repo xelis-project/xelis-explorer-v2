03a1dd4598ec24f7802b0d1c220ecf103d72160b
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-01 12:02:46 -0400

    not needed use set()

--- src/client/pages/mempool/components/state/state.ts
+++ src/client/pages/mempool/components/state/state.ts
@@ -34,22 +34,6 @@ export class MempoolState {
         container_2.appendChild(this.mempool_tx_size_tree_map.box.element);
     }
 
-    reset() {
-        this.mempool_total.set_tx_count(0);
-        this.mempool_total.set_timer(Date.now());
-
-        this.mempool_total.set_fees(0);
-        this.mempool_total.set_size(0);
-
-        this.mempool_tx_type_bars.set_transfer_count(0, 0);
-        this.mempool_tx_type_bars.set_contract_invoke_count(0, 0);
-        this.mempool_tx_type_bars.set_multisig_count(0, 0);
-        this.mempool_tx_type_bars.set_contract_deploy_count(0, 0);
-        this.mempool_tx_type_bars.set_burn_count(0, 0);
-
-        this.mempool_tx_size_tree_map.build_chart();
-    }
-
     set(txs: Transaction[], top_block: Block) {
         let total_fees = 0;
         let total_size = 0;
