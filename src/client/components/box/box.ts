cde56a96b1e73a9ba84b178d65b279dd7e98b2cd
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-04 11:55:49 -0400

    box loading helper func

--- src/client/components/box/box.ts
+++ src/client/components/box/box.ts
@@ -15,4 +15,13 @@ export class Box {
             this.element.classList.remove(`xe-box-loading`);
         }
     }
+
+    static content_loading(element: HTMLElement, loading: boolean, min_height?: string) {
+        if (loading) {
+            if (min_height) element.style.minHeight = min_height;
+            element.classList.add(`xe-box`, `xe-box-loading`);
+        } else {
+            element.classList.remove(`xe-box`, `xe-box-loading`);
+        }
+    }
 }
