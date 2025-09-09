684e65610171cd5b87a3f4361ec7c0c368544748
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-09 16:02:10 -0400

    set default values for items

--- src/client/pages/mempool/components/summary/tx_type_bars.ts
+++ src/client/pages/mempool/components/summary/tx_type_bars.ts
@@ -14,18 +14,23 @@ export class MempoolTxTypeBars {
 
         this.transfers_element = document.createElement(`div`);
         this.element.appendChild(this.transfers_element);
+        this.set_transfer_count(0, 0);
 
         this.invokes_element = document.createElement(`div`);
         this.element.appendChild(this.invokes_element);
+        this.set_contract_invoke_count(0, 0);
 
         this.multisigs_element = document.createElement(`div`);
         this.element.appendChild(this.multisigs_element);
+        this.set_multisig_count(0, 0);
 
         this.deploys_element = document.createElement(`div`);
         this.element.appendChild(this.deploys_element);
+        this.set_contract_deploy_count(0, 0);
 
         this.burns_element = document.createElement(`div`);
         this.element.appendChild(this.burns_element);
+        this.set_burn_count(0, 0);
     }
 
     build_bar(count: number, max: number) {
