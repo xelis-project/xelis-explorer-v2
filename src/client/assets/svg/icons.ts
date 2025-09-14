3d0d1abc6534286d56ebc8b918a52e75df58ecc5
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-14 11:39:18 -0400

    new home icon

--- src/client/assets/svg/icons.ts
+++ src/client/assets/svg/icons.ts
@@ -27,4 +27,12 @@ export const exchange = () => {
     `;
 }
 
-export default { question_mark_shield, tag, exchange };
+export const home = () => {
+    return `
+        <svg width="800px" height="800px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 6V15H6V11C6 9.89543 6.89543 9 8 9C9.10457 9 10 9.89543 10 11V15H15V6L8 0L1 6Z" />
+        </svg>
+    `;
+}
+
+export default { question_mark_shield, tag, exchange, home };
