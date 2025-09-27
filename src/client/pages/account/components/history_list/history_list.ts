5df91248b151cc28ec91c82e9c2686687df668fe
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-27 15:25:04 -0400

    add empty box

--- src/client/pages/account/components/history_list/history_list.ts
+++ src/client/pages/account/components/history_list/history_list.ts
@@ -2,10 +2,12 @@ import { AccountHistory } from '@xelis/sdk/daemon/types';
 import { Container } from '../../../../components/container/container';
 import './history_list.css';
 import { AccountHistoryListItem } from './history_item';
+import icons from '../../../../assets/svg/icons';
 
 export class AccountHistoryList {
     container: Container;
     list_element: HTMLDivElement;
+    empty_element: HTMLDivElement;
 
     constructor(title: string) {
         this.container = new Container();
@@ -15,13 +17,22 @@ export class AccountHistoryList {
         title_element.innerHTML = title;
         this.container.element.appendChild(title_element);
 
+        this.empty_element = document.createElement(`div`);
+        this.empty_element.classList.add(`xe-mempool-txs-list-empty`);
+        this.empty_element.innerHTML = `${icons.empty_box()}<div>No activity.</div>`;
+  
         this.list_element = document.createElement(`div`);
         this.container.element.appendChild(this.list_element);
     }
 
     set(history_list: AccountHistory[]) {
         this.list_element.replaceChildren();
-        history_list.forEach(history_item => this.prepend_history(history_item));
+        if (history_list.length > 0) {
+            history_list.forEach(history_item => this.prepend_history(history_item));
+        } else {
+      this.container.element.appendChild(this.empty_element);
+
+        }
     }
 
     prepend_history(history: AccountHistory) {
