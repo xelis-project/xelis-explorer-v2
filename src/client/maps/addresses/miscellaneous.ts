64f0c6395a9cdf5891e17317b6054f0047b54bc0
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-28 11:02:59 -0400

    add dev wallet

--- src/client/maps/addresses/miscellaneous.ts
+++ src/client/maps/addresses/miscellaneous.ts
@@ -2,11 +2,15 @@ import { AddressDetails } from "./addresses";
 
 export const miscellaneous = {
     "xel:ns5gar4crnhfgj6llyd7x4tfwm0nglkuvqcmjsdz4xdnt90tjf6sqdsm5nx": {
-        name: "Faucet",
+        name: "Team Faucet",
         link: "http://faucet.xelis.io",
     },
     "xel:qtlxcsz58wjdf9ahyfnpym3p0hgyre6cp26urzcppnc65knfdyzsqf845ep": {
-        name: "Tip Bot",
+        name: "Team Tip Bot",
+        link: ""
+    },
+    "xel:vs3mfyywt0fjys0rgslue7mm4wr23xdgejsjk0ld7f2kxng4d4nqqnkdufz": {
+        name: "Dev Wallet",
         link: ""
     }
 } as Record<string, AddressDetails>;
