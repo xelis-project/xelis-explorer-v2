eb68ab7b8d2826ed012ed36916664af0b041bc7c
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-05 16:32:52 -0400

    include flags in a separate folder

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,14 @@ export default defineConfig({
             // overwrite default .html entry
             input: '/src/client/index.ts',
             output: {
+                assetFileNames: (asset_info) => {
+                    const original_filename = asset_info.originalFileNames[0];
+                    if (original_filename && original_filename.startsWith(`node_modules/flag-icons/`)) {
+                        return `flags/[name]-[hash][extname]`
+                    }
+
+                    return 'assets/[name]-[hash][extname]';
+                },
                 manualChunks: {
                     three: ['three', 'camera-controls'],
                     animejs: ['animejs'],
