e1dfa8c26094bf3ff113366beedd342067e997f4
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-06 17:20:15 -0400

    pass number locale options

--- src/client/utils/format_asset.ts
+++ src/client/utils/format_asset.ts
@@ -2,11 +2,11 @@ import { parse_atomic } from "./parse_atomic";
 import assets from "../maps/assets";
 import { reduce_text } from "./reduce_text";
 
-export const format_asset = (asset_hash: string, atomic_amount: number, with_ticker: boolean, locale?: Intl.LocalesArgument) => {
+export const format_asset = (asset_hash: string, atomic_amount: number, with_ticker: boolean, locale?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => {
     const asset = assets[asset_hash];
     if (asset) {
         const amount = parse_atomic(atomic_amount, asset.decimals);
-        let value = `${amount.toLocaleString(locale, { maximumFractionDigits: asset.decimals })}`;
+        let value = `${amount.toLocaleString(locale, { maximumFractionDigits: asset.decimals, ...options })}`;
         if (with_ticker) value += ` ${asset.ticker}`;
         return value;
     }
