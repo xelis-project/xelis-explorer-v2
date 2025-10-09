adf63448b23b98a6006d5250996bec01a7e4c962
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-09 13:23:51 -0400

    fix contract output data display

--- src/client/pages/transaction/components/contract_outputs/contract_outputs.ts
+++ src/client/pages/transaction/components/contract_outputs/contract_outputs.ts
@@ -18,15 +18,7 @@ export class TransactionContractOutputs {
 
         const box = new Box();
         const json_viewer = new JsonViewer();
-
-        const data = {} as any;
-        outputs.forEach((output) => {
-            Object.keys(output).forEach(key => {
-                data[key] = (output as any)[key];
-            });
-        });
-
-        json_viewer.set_data(data);
+        json_viewer.set_data(JSON.parse(JSON.stringify(outputs)));
         box.element.appendChild(json_viewer.element);
         this.container.element.appendChild(box.element);
     }
