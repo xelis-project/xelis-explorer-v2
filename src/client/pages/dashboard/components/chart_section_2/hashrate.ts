a7dc05165fc94f563d86d2f3498d230efdfea842
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-11 11:16:29 -0400

    make sure data is not empty

--- src/client/pages/dashboard/components/chart_section_2/hashrate.ts
+++ src/client/pages/dashboard/components/chart_section_2/hashrate.ts
@@ -86,56 +86,58 @@ export class DashboardHashRate {
             .attr('fill', d => color(d.y))
             .attr('stroke', 'none');
 
-        const min_data = data.reduce((a, b) => (a.y < b.y ? a : b));
-        const max_data = data.reduce((a, b) => (a.y > b.y ? a : b));
+        if (data.length > 0) {
+            const min_data = data.reduce((a, b) => (a.y < b.y ? a : b));
+            const max_data = data.reduce((a, b) => (a.y > b.y ? a : b));
 
-        const tooltip = svg
-            .selectAll(`g`)
-            .data<DataPoint>([min_data, max_data])
-            .enter()
-            .append(`g`);
+            const tooltip = svg
+                .selectAll(`g`)
+                .data<DataPoint>([min_data, max_data])
+                .enter()
+                .append(`g`);
 
-        tooltip
-            .append(`text`)
-            .text(d => format_hashrate(d.y, info.block_time_target))
-            .each(function (d) {
-                const self = this as SVGTextElement;
+            tooltip
+                .append(`text`)
+                .text(d => format_hashrate(d.y, info.block_time_target))
+                .each(function (d) {
+                    const self = this as SVGTextElement;
 
-                const text_box = self.getBBox();
-                const rect_margin = { top: 2, left: 10, bottom: 2, right: 10 };
-                const rect_width = text_box.width + rect_margin.left + rect_margin.right;
-                const rect_height = text_box.height + rect_margin.top + rect_margin.bottom;
+                    const text_box = self.getBBox();
+                    const rect_margin = { top: 2, left: 10, bottom: 2, right: 10 };
+                    const rect_width = text_box.width + rect_margin.left + rect_margin.right;
+                    const rect_height = text_box.height + rect_margin.top + rect_margin.bottom;
 
-                const text_pos = { x: 0, y: 0 };
-                text_pos.x = Math.min(chart_rect.width - rect_width - 10, Math.max(0, x_scale(d.x) - text_box.width / 2));
-                text_pos.y = Math.max(rect_height / 2, y_scale(d.y));
-                d.text = text_pos;
+                    const text_pos = { x: 0, y: 0 };
+                    text_pos.x = Math.min(chart_rect.width - rect_width - 10, Math.max(0, x_scale(d.x) - text_box.width / 2));
+                    text_pos.y = Math.max(rect_height / 2, y_scale(d.y));
+                    d.text = text_pos;
 
-                const text_rect = {} as DOMRect;
-                text_rect.x = text_pos.x - rect_margin.left + 2;
-                text_rect.y = text_pos.y - (rect_height / 2) - 5;
-                text_rect.width = rect_width;
-                text_rect.height = rect_height;
+                    const text_rect = {} as DOMRect;
+                    text_rect.x = text_pos.x - rect_margin.left + 2;
+                    text_rect.y = text_pos.y - (rect_height / 2) - 5;
+                    text_rect.width = rect_width;
+                    text_rect.height = rect_height;
 
-                d.text_rect = text_rect;
-            })
-            .attr("x", d => {
-                return d.text ? d.text.x : 0;
-            })
-            .attr("y", d => {
-                return d.text ? d.text.y : 0;
-            })
-            .style('font-size', '1rem')
-            //.style("text-anchor", "middle") // using js to anchor text in the middle to bound text within the canvas
-            .style('font-weight', `bold`)
-            .style('fill', 'rgba(0, 0, 0, 1)');
+                    d.text_rect = text_rect;
+                })
+                .attr("x", d => {
+                    return d.text ? d.text.x : 0;
+                })
+                .attr("y", d => {
+                    return d.text ? d.text.y : 0;
+                })
+                .style('font-size', '1rem')
+                //.style("text-anchor", "middle") // using js to anchor text in the middle to bound text within the canvas
+                .style('font-weight', `bold`)
+                .style('fill', 'rgba(0, 0, 0, 1)');
 
-        tooltip.insert(`rect`, ":first-child")
-            .attr("x", d => d.text_rect ? d.text_rect.x : 0)
-            .attr("y", d => d.text_rect ? d.text_rect.y : 0)
-            .attr("width", d => d.text_rect ? d.text_rect.width : 0)
-            .attr("height", d => d.text_rect ? d.text_rect.height : 0)
-            .style('fill', 'rgba(2, 255, 209)');
+            tooltip.insert(`rect`, ":first-child")
+                .attr("x", d => d.text_rect ? d.text_rect.x : 0)
+                .attr("y", d => d.text_rect ? d.text_rect.y : 0)
+                .attr("width", d => d.text_rect ? d.text_rect.width : 0)
+                .attr("height", d => d.text_rect ? d.text_rect.height : 0)
+                .style('fill', 'rgba(2, 255, 209)');
+        }
     }
 
     set(info: GetInfoResult, blocks: Block[]) {
