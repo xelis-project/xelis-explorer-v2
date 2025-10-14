0947510568aa367962ce599c9c6931ec01324795
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-14 17:45:51 -0400

    update hashrate chart instead of rebuild + add translate anim

--- src/client/pages/dashboard/components/chart_section_2/hashrate.ts
+++ src/client/pages/dashboard/components/chart_section_2/hashrate.ts
@@ -13,12 +13,22 @@ interface DataPoint {
 export class DashboardHashRate {
     box_chart: BoxChart;
 
+    blocks!: Block[];
+    info!: GetInfoResult;
+    chart?: {
+        node: d3.Selection<SVGGElement, unknown, null, undefined>;
+        width: number;
+        height: number;
+        rect: DOMRect
+    }
+
     constructor() {
         this.box_chart = new BoxChart();
         this.box_chart.element_title.innerHTML = `HASHRATE`;
 
         window.addEventListener(`resize`, () => {
-            //this.build_chart();
+            this.create_chart();
+            this.update_chart();
         });
     }
 
@@ -27,36 +37,43 @@ export class DashboardHashRate {
         this.box_chart.element_value.innerHTML = hashrate;
     }
 
-    build_chart(blocks: Block[], info: GetInfoResult) {
-        const data = blocks
-            .filter((item, i) => blocks.map(x => x.height).indexOf(item.height) === i) // no duplicate height - remove side block
-            .map((block) => {
-                return { x: block.height, y: parseInt(block.difficulty) };
-            });
-
-        const chart_margin = { top: 10, right: 10, bottom: 10, left: 10 };
-        const chart_rect = this.box_chart.element_content.getBoundingClientRect();
-        const width = chart_rect.width - chart_margin.left - chart_margin.right;
-        const height = 250 - chart_margin.top - chart_margin.bottom;
-
+    create_chart() {
         this.box_chart.element_content.replaceChildren();
-        const svg = d3
+
+        const margin = { top: 10, right: 10, bottom: 10, left: 10 };
+        const rect = this.box_chart.element_content.getBoundingClientRect();
+        const width = rect.width - margin.left - margin.right;
+        const height = 250 - margin.top - margin.bottom;
+
+        const node = d3
             .select(this.box_chart.element_content)
             .append("svg")
-            .attr("width", width + chart_margin.left + chart_margin.right)
-            .attr("height", height + chart_margin.top + chart_margin.bottom)
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform",
-                "translate(" + chart_margin.left + "," + chart_margin.top + ")");
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        this.chart = { node, width, height, rect };
+    }
+
+    update_chart() {
+        if (!this.chart) return;
+
+        const data = this.blocks
+            .filter((item, i) => this.blocks.map(x => x.height).indexOf(item.height) === i) // no duplicate height - remove side block
+            .map((block) => {
+                return { x: block.height, y: parseInt(block.difficulty) };
+            }) as DataPoint[];
 
         const x_scale = d3.scaleLinear()
             .domain(d3.extent(data, d => d.x) as [number, number])
-            .range([0, width]);
+            .range([0, this.chart.width]);
 
         const y_scale = d3.scaleLinear()
             .domain([d3.min(data, d => d.y)!, d3.max(data, d => d.y)!])
             .nice()
-            .range([height, 0]);
+            .range([this.chart.height, 0]);
 
         const line = d3.line<DataPoint>()
             .x(d => x_scale(d.x))
@@ -67,38 +84,62 @@ export class DashboardHashRate {
             .domain(data.map(d => d.y))
             .range(d3.quantize(t => d3.interpolateRgb(`#02ffcf`, `#ff00aa`)(t * 0.5), data.length));
 
-        svg
-            .append('path')
-            .datum(data)
-            .attr('fill', `none`)
-            .attr('stroke', d => `rgba(2, 255, 209, 0.3)`)
-            //.attr('stroke', d => color(d.y))
-            .attr('stroke-width', 5)
-            .attr('d', line);
+        const path = this.chart.node
+            .selectAll(`path`)
+            .data([data])
+            .attr('d', (d) => line(d))
 
-        svg
+        path.enter()
+            .append('path')
+            .attr('fill', `none`)
+            .style('stroke-width', 5)
+            .attr('stroke', `rgba(2, 255, 209, 0.3)`)
+            .attr('d', (d) => line(d));
+
+        path.exit().remove();
+
+        this.chart.node.select(`path`)
+            .attr("transform", null)
+            .transition()
+            .duration(500)
+            .attr("transform", "translate(-10, 0)");
+
+        this.chart.node
             .selectAll(`circle`)
             .data(data)
             .join(`circle`)
             .attr('cx', d => x_scale(d.x))
             .attr('cy', d => y_scale(d.y))
             .attr('r', 5)
+            .attr('stroke', 'none')
+            //`.transition()
+            //.duration(500)
             .attr('fill', d => color(d.y))
-            .attr('stroke', 'none');
+            .attr("transform", null)
+            .transition()
+            .duration(500)
+            .attr("transform", "translate(-10,0)");
 
+        // update tooltip
         if (data.length > 0) {
             const min_data = data.reduce((a, b) => (a.y < b.y ? a : b));
             const max_data = data.reduce((a, b) => (a.y > b.y ? a : b));
 
-            const tooltip = svg
-                .selectAll(`g`)
+            const chart_rect_width = this.chart.rect.width;
+            this.chart.node
+                .selectAll(`.tooltip`)
+                .remove();
+
+            const tooltip = this.chart.node
+                .selectAll(`.tooltip`)
                 .data<DataPoint>([min_data, max_data])
                 .enter()
-                .append(`g`);
+                .append(`g`)
+                .attr(`class`, `tooltip`);
 
             tooltip
                 .append(`text`)
-                .text(d => format_hashrate(d.y, info.block_time_target))
+                .text(d => format_hashrate(d.y, this.info.block_time_target))
                 .each(function (d) {
                     const self = this as SVGTextElement;
 
@@ -108,7 +149,7 @@ export class DashboardHashRate {
                     const rect_height = text_box.height + rect_margin.top + rect_margin.bottom;
 
                     const text_pos = { x: 0, y: 0 };
-                    text_pos.x = Math.min(chart_rect.width - rect_width - 10, Math.max(0, x_scale(d.x) - text_box.width / 2));
+                    text_pos.x = Math.min(chart_rect_width - rect_width - 10, Math.max(0, x_scale(d.x) - text_box.width / 2));
                     text_pos.y = Math.max(rect_height / 2, y_scale(d.y));
                     d.text = text_pos;
 
@@ -129,19 +170,29 @@ export class DashboardHashRate {
                 .style('font-size', '1rem')
                 //.style("text-anchor", "middle") // using js to anchor text in the middle to bound text within the canvas
                 .style('font-weight', `bold`)
-                .style('fill', 'rgba(0, 0, 0, 1)');
+                .style('fill', 'rgba(0, 0, 0, 1)')
+                .attr(`transform`, `translate(-10, 0)`);
 
             tooltip.insert(`rect`, ":first-child")
                 .attr("x", d => d.text_rect ? d.text_rect.x : 0)
                 .attr("y", d => d.text_rect ? d.text_rect.y : 0)
                 .attr("width", d => d.text_rect ? d.text_rect.width : 0)
                 .attr("height", d => d.text_rect ? d.text_rect.height : 0)
-                .style('fill', 'rgba(2, 255, 209)');
+                .style('fill', 'rgba(2, 255, 209)')
+                .attr(`transform`, `translate(-10, 0)`);
         }
     }
 
     set(info: GetInfoResult, blocks: Block[]) {
+        this.info = info;
+        this.blocks = blocks;
+
         this.set_hashrate(info);
-        this.build_chart(blocks, info);
+
+        if (!this.chart) {
+            this.create_chart();
+        }
+
+        this.update_chart();
     }
 }
