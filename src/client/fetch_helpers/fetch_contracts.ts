8c3d086b8da1ddd758bb931205282c47ea989371
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-21 12:17:09 -0400

    remove contract module (already inside transaction data) and add contract deploy block fetch

--- src/client/fetch_helpers/fetch_contracts.ts
+++ src/client/fetch_helpers/fetch_contracts.ts
@@ -1,82 +1,93 @@
 import { RPCRequest, } from "@xelis/sdk/rpc/types";
 import { XelisNode } from "../app/xelis_node";
-import { RPCMethod as DaemonRPCMethod, GetContractBalanceParams, GetContractBalanceResult, GetContractModuleParams, GetContractModuleResult, Transaction, TransactionResponse } from "@xelis/sdk/daemon/types";
+import { Block, RPCMethod as DaemonRPCMethod, GetBlockByHashParams, GetContractBalanceParams, GetContractBalanceResult, GetContractModuleParams, GetContractModuleResult, Transaction, TransactionResponse } from "@xelis/sdk/daemon/types";
 import { XELIS_ASSET } from "@xelis/sdk/config";
 
 export interface ContractInfo {
     transaction: TransactionResponse;
     module: GetContractModuleResult;
     balance?: GetContractBalanceResult;
+    block: Block;
 }
 
 export const fetch_contracts = async (contracts: string[]) => {
     const node = XelisNode.instance();
 
-    if (contracts.length > 6) {
-        throw "limit of 6 contracts";
+    if (contracts.length > 10) {
+        throw "limit of 10 contracts";
     }
 
-    const requests = [] as RPCRequest[];
-
-    contracts.forEach((contract) => {
-        requests.push({
-            method: DaemonRPCMethod.GetTransaction,
-            params: { hash: contract }
-        });
-
-        requests.push({
-            method: DaemonRPCMethod.GetContractModule,
-            params: { contract } as GetContractModuleParams
-        });
-
-        requests.push({
-            method: DaemonRPCMethod.GetContractBalance,
-            params: { contract, asset: XELIS_ASSET } as GetContractBalanceParams
-        })
-    });
-
-    const res = await node.rpc.batchRequest(requests);
-
     const contracts_info = [] as ContractInfo[];
-    let contract_index = 0;
-    res.forEach((result, i) => {
-        const data_index = i % 3;
-        switch (data_index) {
-            // GetTransaction
-            case 0:
-                if (result instanceof Error) {
-                    throw result;
-                }
 
-                contracts_info[contract_index] = {
-                    transaction: result
-                } as ContractInfo;
+    {
+        const requests = [] as RPCRequest[];
+        contracts.forEach((contract) => {
+            requests.push({
+                method: DaemonRPCMethod.GetTransaction,
+                params: { hash: contract }
+            });
 
-                break;
-            // GetContractModule
-            case 1:
-                if (result instanceof Error) {
-                    throw result;
-                }
+            requests.push({
+                method: DaemonRPCMethod.GetContractBalance,
+                params: { contract, asset: XELIS_ASSET } as GetContractBalanceParams
+            });
+        });
 
-                contracts_info[contract_index].module = result as GetContractModuleResult;
+        const res = await node.rpc.batchRequest(requests);
+        let contract_index = 0;
+        res.forEach((result, i) => {
+            const data_index = i % 2;
+            switch (data_index) {
+                // GetTransaction
+                case 0:
+                    if (result instanceof Error) {
+                        throw result;
+                    }
 
-                break;
-            // GetContractBalance (XEL)
-            case 2:
-                if (result instanceof Error) {
-                    // API returns an error if the contract does not have a balance
-                    contracts_info[contract_index].balance = undefined;
-                } else {
-                    contracts_info[contract_index].balance = result as GetContractBalanceResult;
-                }
+                    contracts_info[contract_index] = {
+                        transaction: result
+                    } as ContractInfo;
 
-                contract_index++;
-                break;
-            default:
-                throw "should not hit";
-        }
-    });
+                    break;
+                // GetContractBalance (XEL)
+                case 1:
+                    if (result instanceof Error) {
+                        // API returns an error if the contract does not have a balance
+                        contracts_info[contract_index].balance = undefined;
+                    } else {
+                        contracts_info[contract_index].balance = result as GetContractBalanceResult;
+                    }
+
+                    contract_index++;
+                    break;
+                default:
+                    throw "should not hit";
+            }
+        });
+    }
+
+    {
+        const requests = [] as RPCRequest[];
+
+        contracts_info.forEach((info) => {
+            if (info.transaction.executed_in_block) {
+                requests.push({
+                    method: DaemonRPCMethod.GetBlockByHash,
+                    params: { hash: info.transaction.executed_in_block } as GetBlockByHashParams
+                });
+            }
+        });
+
+        const res = await node.rpc.batchRequest(requests);
+        res.forEach((result, i) => {
+            if (result instanceof Error) {
+                throw result;
+            }
+
+            contracts_info[i].block = result as Block;
+        });
+    }
+
 
     return contracts_info;
 }
