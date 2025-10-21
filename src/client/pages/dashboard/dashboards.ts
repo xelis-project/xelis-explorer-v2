1283d371c34db753bba1f5f7fe59ba4123974aff
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-10-21 12:51:03 -0400

    await set peers func

--- src/client/pages/dashboard/dashboards.ts
+++ src/client/pages/dashboard/dashboards.ts
@@ -303,7 +303,7 @@ export class DashboardPage extends Page {
         this.dashboard_peers.peers_map.overlay_loading.set_loading(true);
         const peers_result = await node.rpc.getPeers();
         const peers_locations = await this.dashboard_peers.peers_map.fetch_peers_locations(peers_result.peers);
-        this.dashboard_peers.peers_map.set(peers_locations);
+        await this.dashboard_peers.peers_map.set(peers_locations);
         this.dashboard_peers.peers_map.overlay_loading.set_loading(false);
     }
 
