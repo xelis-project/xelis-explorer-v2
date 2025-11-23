74179f485e7e53f367250433803fe9bd9411c6d0
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-11-23 11:38:30 -0500

    new previous/next block btns

---
+++ src/client/pages/block/components/next_block_btns/next_block_btns.ts
@@ -0,0 +1,40 @@
+import { GetInfoResult } from '@xelis/sdk/daemon/types';
+import icons from '../../../../assets/svg/icons';
+import './next_block_btns.css';
+
+export class BlockNextBlockBtns {
+    element: HTMLDivElement;
+    previous_block: HTMLAnchorElement;
+    next_block: HTMLAnchorElement;
+
+    constructor() {
+        this.element = document.createElement(`div`);
+        this.element.classList.add(`xe-block-next-block-btns`);
+
+        this.previous_block = document.createElement(`a`);
+        this.element.appendChild(this.previous_block);
+
+        this.next_block = document.createElement(`a`);
+        this.element.appendChild(this.next_block);
+    }
+
+    set(info: GetInfoResult, topoheight?: number) {
+        if (topoheight) {
+            this.element.style.removeProperty(`display`);
+            const previous_topo = Math.max(0, topoheight - 1);
+            this.previous_block.innerHTML = `${icons.caret_down()} Previous (${previous_topo})`;
+            this.previous_block.href = `/topo/${previous_topo}`;
+
+            const next_topo = topoheight + 1;
+            if (info.topoheight >= next_topo) {
+                this.next_block.style.removeProperty(`display`);
+                this.next_block.innerHTML = `Next (${next_topo}) ${icons.caret_down()}`;
+                this.next_block.href = `/topo/${next_topo}`;
+            } else {
+                this.next_block.style.display = `none`;
+            }
+        } else {
+            this.element.style.display = `none`;
+        }
+    }
+}
