171c3bb6cc44ec71603e10985995960603ca0916
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-11-27 10:18:05 -0500

    refactor find txs helper

--- scripts/find_txs.ts
+++ scripts/find_txs.ts
@@ -1,23 +1,34 @@
 import { TESTNET_NODE_RPC } from '@xelis/sdk/config';
 import { RPC as DaemonRPC } from '@xelis/sdk/daemon/rpc'
+import fs from 'fs';
 
 async function main() {
     const daemonRPC = new DaemonRPC(TESTNET_NODE_RPC);
 
-    for (let i = 900; i < 20000; i += 20) {
+    let start = 0;
+    let end = await daemonRPC.getHeight();
+    let output_path = `./txs.json`;
+    let txs = [] as any[];
+
+    for (let i = start; i < end; i += 20) {
         const blocks = await daemonRPC.getBlocksRangeByHeight({
             start_height: i,
-            end_height: i + 19
+            end_height: Math.min(end, i + 19)
         });
 
-        console.log(i);
+        console.log(`Block ${i}`);
+
         for (let a = 0; a < blocks.length; a++) {
             const block = blocks[a];
-            if (block.txs_hashes.length > 0) {
-                console.log(block.txs_hashes)
+            const tx_count = block.txs_hashes.length;
+            if (tx_count > 0) {
+                txs = [...txs, ...block.txs_hashes];
+                console.log(`${tx_count} txs`);
             }
         }
     }
+
+    fs.writeFileSync(output_path, JSON.stringify(txs), { encoding: `utf-8` });
 }
 
 main();
