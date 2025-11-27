e3b63b9e312e3734f2a0f9085223fa6c9b983aac
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-11-27 18:07:03 -0500

    refactor code

--- src/client/components/overlay_loading/overlay_loading.ts
+++ src/client/components/overlay_loading/overlay_loading.ts
@@ -5,20 +5,24 @@ export class OverlayLoading {
 
     constructor() {
         this.element = document.createElement(`div`);
+        this.element.classList.add(`xe-overlay-loading`);
     }
 
     set_loading(loading: boolean) {
         if (loading) {
-            const loading_element = document.createElement(`div`);
-            loading_element.classList.add(`xe-overlay-loading`);
-            loading_element.innerHTML = `
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                `;
-            this.element.appendChild(loading_element);
+            const wrap = document.createElement(`div`);
+            wrap.classList.add(`xe-overlay-loading-wrap`);
+
+            const content = document.createElement(`div`);
+            content.classList.add(`xe-overlay-loading-content`);
+            wrap.appendChild(content);
+
+            for (let i = 0; i < 3; i++) {
+                const line = document.createElement(`div`);
+                content.appendChild(line);
+            }
+
+            this.element.appendChild(wrap);
         } else {
             this.element.replaceChildren();
         }
