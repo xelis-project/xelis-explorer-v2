ed009b0526d57c7a913971278a67870390957255
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-12-08 18:01:59 -0500

    new icons

--- src/client/assets/svg/icons.ts
+++ src/client/assets/svg/icons.ts
@@ -209,10 +209,43 @@ export const tokens = () => {
     `;
 }
 
+export const wallet = () => {
+    return `
+        <svg fill="currentColor" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22,17a4,4,0,1,1-4-4A4,4,0,0,1,22,17ZM21,3H4A2,2,0,0,0,2,5V19a2,2,0,0,0,2,2h9.54A5.99,5.99,0,0,1,22,12.54V8a1,1,0,0,0-1-1H5A1,1,0,0,1,5,5H22V4A1,1,0,0,0,21,3Z"/>
+        </svg>
+    `;
+}
+
+export const arrow = () => {
+    return `
+        <svg fill="currentColor" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18.364 8.95l-5.657-5.657a1 1 0 0 0-1.414 0L5.636 8.95a1 1 0 0 0 1.414 1.414L11 6.414V20a1 1 0 0 0 2 0V6.414l3.95 3.95a1 1 0 0 0 1.414-1.414z"/>
+        </svg>
+    `;
+}
+
+export const dual_arrow = () => {
+    return `
+        <svg fill="currentColor" width="800px" height="800px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2 10l4-4m-4 4l4 4m-4-4h16.5m0 0l-4-4m4 4l-4 4"/>
+        </svg>
+    `;
+}
+
+export const lock = () => {
+    return `
+        <svg fill="currentColor" width="800px" height="800px" viewBox="0 0 56 56" xmlns="http://www.w3.org/2000/svg">
+            <path d="M 28.0000 4.2578 C 21.4609 4.2578 15.4844 8.9219 15.4844 18.5078 L 15.4844 24.1328 C 12.9531 24.4375 11.7109 25.9610 11.7109 28.9610 L 11.7109 46.8438 C 11.7109 50.2188 13.2578 51.7422 16.375 51.7422 L 39.625 51.7422 C 42.7422 51.7422 44.2891 50.2188 44.2891 46.8438 L 44.2891 28.9375 C 44.2891 25.9375 43.0469 24.3437 40.5156 24.0625 L 40.5156 18.5078 C 40.5156 8.9219 34.5391 4.2578 28.0000 4.2578 Z M 19.2578 17.9922 C 19.2578 11.4532 23.1484 7.8672 28.0000 7.8672 C 32.8515 7.8672 36.7422 11.4532 36.7422 17.9922 L 36.7422 24.0391 L 19.2578 24.0625 Z"/>
+        </svg>
+    `;
+}
+
 export default {
     question_mark_shield, tag, exchange,
     home, empty_box, menu, dashboard, blocks,
     contract, compute, block_graph, network,
     search, connect, user, burn, cog, menu_alt,
-    reset, save, caret_down, close, tokens
+    reset, save, caret_down, close, tokens,
+    wallet, arrow, dual_arrow, lock
 };
