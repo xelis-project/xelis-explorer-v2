8927365e6e5838b00715db393ee2cc7651644666
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-12-15 13:10:05 -0500

    impl asset caching

--- src/client/utils/ws_format_asset.ts
+++ src/client/utils/ws_format_asset.ts
@@ -1,7 +1,10 @@
+import { AssetData } from "@xelis/sdk/daemon/types";
 import { get_assets } from "../data/assets";
 import { parse_atomic } from "./parse_atomic";
 import { reduce_text } from "./reduce_text";
 import DaemonWS from '@xelis/sdk/daemon/websocket';
+
+const asset_cache = new Map<string, AssetData>();
 
 export const ws_format_asset = async (ws: DaemonWS, asset_hash: string, atomic_amount: number, locales?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => {
     const format = (decimals: number, ticker?: string) => {
@@ -18,7 +21,16 @@ export const ws_format_asset = async (ws: DaemonWS, asset_hash: string, atomic_a
         return `${format(local_asset.decimals, local_asset.ticker)}`;
     }
 
-    const asset_data = await ws.methods.getAsset({ asset: asset_hash });
+    let asset_data = asset_cache.get(asset_hash);
+    if (!asset_data) {
+        try {
+            asset_data = await ws.methods.getAsset({ asset: asset_hash });
+            asset_cache.set(asset_hash, asset_data);
+        } catch {
+            // can't get data
+        }
+    }
+
     if (asset_data) {
         return `${format(asset_data.decimals, asset_data.ticker)}`;
     }
