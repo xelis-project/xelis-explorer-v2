1c84e213ebb12282ac30b8e4601ac8a2bae5811f
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2026-01-08 21:53:06 -0500

    change format_asset logic

--- src/client/utils/format_asset.ts
+++ src/client/utils/format_asset.ts
@@ -1,16 +1,8 @@
 import { parse_atomic } from "./parse_atomic";
-import { reduce_text } from "./reduce_text";
-import { get_assets } from "../data/assets";
 
-export const format_asset = (asset_hash: string, atomic_amount: number, with_ticker: boolean, locale?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => {
-    const assets = get_assets();
-    const asset = assets[asset_hash];
-    if (asset) {
-        const amount = parse_atomic(atomic_amount, asset.decimals);
-        let value = `${amount.toLocaleString(locale, { maximumFractionDigits: asset.decimals, ...options })}`;
-        if (with_ticker) value += ` ${asset.ticker}`;
-        return value;
-    }
-
-    return `${atomic_amount} (${reduce_text(asset_hash)})`;
+export const format_asset = (atomic_amount: number, decimals: number, ticker: string, locale?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => {
+    const amount = parse_atomic(atomic_amount, decimals);
+    let value = `${amount.toLocaleString(locale, { maximumFractionDigits: decimals, ...options })}`;
+    if (ticker) value += ` ${ticker}`;
+    return value;
 }
--- src/client/utils/format_xel.ts
+++ src/client/utils/format_xel.ts
@@ -1,6 +1,5 @@
-import { XELIS_ASSET } from "@xelis/sdk/config";
 import { format_asset } from "./format_asset";
 
 export const format_xel = (atomic_amount: number, with_ticker: boolean, locale?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => {
-    return format_asset(XELIS_ASSET, atomic_amount, with_ticker, locale, options);
+    return format_asset(atomic_amount, 8, with_ticker ? `XEL` : ``, locale, options);
 }
